5c042a57bc13e1d2f64680a68a7b988dd7443e03
Jiacong Xu <jiacong.xu@foxmail.com>
2017-10-11 19:16:49 -0700

    Added minimum amount of models to start coding the scaffold.

---
+++ Assets/Scripts/Models/Drawable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Antigear.Graph.Model {
+    /// <summary>
+    /// Maintains metadata for each component of the graph and provides common
+    /// methods such as translation and rotation.
+    /// </summary>
+    [Serializable]
+    public abstract class Drawable {
+        public string name;
+
+        public DateTime timeLastSelected;
+
+        public Vector2 rotationPivot;
+    }
+}
---
+++ Assets/Scripts/Models/StraightLine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Antigear.Graph.Model {
+    /// <summary>
+    /// Models a simple straight line.
+    /// </summary>
+    [Serializable]
+    public class StraightLine : Line {
+        Vector2 startPoint;
+        Vector2 endPoint;
+    }
+}
