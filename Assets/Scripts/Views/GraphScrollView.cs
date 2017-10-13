8a3e7cd50a2ac28984ae101b87b6737b1d396059
Jiacong Xu <jiacong.xu@foxmail.com>
2017-10-12 23:10:11 -0700

    Removed I2 for now and further improved landing screen.

--- Assets/Scripts/Views/GraphScrollView.cs
+++ Assets/Scripts/Views/GraphScrollView.cs
@@ -10,7 +10,16 @@ namespace Antigear.Graph {
     public class GraphScrollView : MonoBehaviour {
         public GridLayoutGroup gridLayoutGroup;
 
-        public float cardWidthHeightRatio = 0.6f;
+        public float cardWidthHeightRatio = 0.8f;
+        public float minInchesPerColumn = 2.3f;
+        public int minColumns = 2;
+        public int maxColumns = 5;
+
+        void Start() {
+            int n = (int)(DeviceDiagonalSize() / minInchesPerColumn);
+            gridLayoutGroup.constraintCount = 
+                Mathf.Max(minColumns, Mathf.Min(maxColumns, n));
+        }
 
         void Update() {
             RectTransform t = transform as RectTransform;
@@ -22,6 +31,15 @@ namespace Antigear.Graph {
             v.x = (width - space) / gridLayoutGroup.constraintCount;
             v.y = v.x / cardWidthHeightRatio;
             gridLayoutGroup.cellSize = v;
+        }
+
+        public static float DeviceDiagonalSize() {
+            float screenWidth = Screen.width / Screen.dpi;
+            float screenHeight = Screen.height / Screen.dpi;
+            float diagonalInches = Mathf.Sqrt(
+                Mathf.Pow(screenWidth, 2) + Mathf.Pow(screenHeight, 2));
+            
+            return diagonalInches;
         }
     }
 }
